2c5241f877c6dc0a5a482a63e090ef9599836d37
Christopher Thorne <libcthorne@gmail.com>
2017-10-22 12:54:29 +0100

    Add paginated list of entries to search index

--- searcher/views.py
+++ searcher/views.py
@@ -1,4 +1,22 @@
+from django.core.paginator import (
+    EmptyPage,
+    PageNotAnInteger,
+    Paginator,
+)
 from django.shortcuts import render
 
+from importer.models import DictionaryEntry
+
 def index(request):
-    return render(request, 'searcher/index.html')
+    all_entries = DictionaryEntry.objects.all()
+    paginator = Paginator(all_entries, per_page=20)
+
+    page = request.GET.get('page')
+    try:
+        entries = paginator.page(page)
+    except PageNotAnInteger:
+        entries = paginator.page(1)
+    except EmptyPage:
+        entries = paginator.page(paginator.num_pages)
+
+    return render(request, 'searcher/index.html', {'entries': entries})
