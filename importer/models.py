70b6ff9c557f1b0c7e3bddf4d3a812a6561d63b9
Christopher Thorne <libcthorne@gmail.com>
2017-10-23 20:51:55 +0100

    Add construction of basic inverted index
     - Index words on import
     - Normalize inverted index words
     - Store position of indexed word

--- importer/models.py
+++ importer/models.py
@@ -1,4 +1,9 @@
+import string
+import unicodedata
+
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class DictionaryImportRequest(models.Model):
     started = models.BooleanField(default=False)
@@ -19,3 +24,51 @@ class DictionaryEntry(models.Model):
 
     def __str__(self):
         return self.edict_data
+
+@receiver(post_save, sender=DictionaryEntry)
+def post_save(sender, instance, **kwargs):
+    edict_data = str(instance.edict_data)
+
+    # Index entry words
+    for word in edict_data.split(' '):
+        start_position = edict_data.index(word)
+        word = InvertedIndexWord.normalize(word)
+        if not word:
+            continue # word is not indexable
+
+        print("Indexing %s under %s (start:%d)" % (edict_data, word, start_position))
+
+        inverted_index_word, _ = InvertedIndexWord.objects.get_or_create(word=word)
+        inverted_index_entry = InvertedIndexEntry(
+            word=inverted_index_word,
+            dictionary_entry=instance,
+            start_position=start_position,
+        )
+        inverted_index_entry.save()
+
+class InvertedIndexWord(models.Model):
+    word = models.CharField(max_length=128)
+
+    @staticmethod
+    def normalize(word):
+        # strip trailing whitespace
+        word = word.rstrip()
+        # make case-insensitive
+        word = word.lower()
+        # normalize for full-width to half-width conversion, among other things
+        word = unicodedata.normalize('NFKC', word)
+        # remove punctuation characters
+        word = ''.join(c for c in word if c not in string.punctuation)
+
+        return word
+
+    def __str__(self):
+        return self.word
+
+class InvertedIndexEntry(models.Model):
+    word = models.ForeignKey(InvertedIndexWord, on_delete=models.CASCADE, related_name='entries')
+    dictionary_entry = models.ForeignKey(DictionaryEntry, on_delete=models.CASCADE)
+    start_position = models.PositiveIntegerField()
+
+    def __str__(self):
+        return "Index of '{}' for {}".format(self.word, self.dictionary_entry)
