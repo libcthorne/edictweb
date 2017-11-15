30df8a37a29117f519ed43cde0137404f454e669
Christopher Thorne <libcthorne@gmail.com>
2017-11-15 21:02:42 +0000

    Possible API fix

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,12 @@
-from django.conf.urls import include, url
-from rest_framework import routers
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
 
 app_name = 'api'
 
-router = routers.DefaultRouter()
-router.register(r'entries', views.DictionaryEntryViewSet, base_name='entries')
-
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    url(r'^entries/$', views.DictionaryEntryList.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from importer.models import DictionaryEntry
 from searcher.queries import search_entries
 from .serializers import DictionaryEntrySerializer
 
-
-class DictionaryEntryViewSet(viewsets.ReadOnlyModelViewSet):
-    serializer_class = DictionaryEntrySerializer
-
-    def get_queryset(self):
-        query = self.request.query_params.get('query', None)
+class DictionaryEntryList(APIView):
+    def get(self, request, format=None):
+        query = request.query_params.get('query', None)
         if query:
             matching_entries, _, _ = search_entries(query, paginate=False)
-            return matching_entries
         else:
-            return DictionaryEntry.objects.all()
+            matching_entries = DictionaryEntry.objects.all()
+
+        serializer = DictionaryEntrySerializer(matching_entries, many=True)
+        return Response(serializer.data)
