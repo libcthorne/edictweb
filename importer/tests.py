d1c47b280b9549edd9e9e18d50ac6172a528c5fd
Christopher Thorne <libcthorne@gmail.com>
2017-12-12 20:10:09 +0000

    Add DictionaryEntry model tests

--- importer/tests.py
+++ importer/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import (
+    DictionaryEntry,
+    DictionaryImportRequest,
+)
+from .util import meta_info_to_label
+
+def create_stub_import_request():
+    return DictionaryImportRequest.objects.create(
+        started=True,
+        completed=True,
+    )
+
+class DictionaryEntryTests(TestCase):
+    def test_meta_labels(self):
+        entry = DictionaryEntry.objects.create(
+            jp_text="犬",
+            en_text="dog",
+            meta_text="(n) (uk) {animal}",
+            sequence_number=100,
+            source_import_request=create_stub_import_request(),
+        )
+
+        self.assertCountEqual(entry.meta_labels, [
+            meta_info_to_label('n'),
+            meta_info_to_label('uk'),
+            meta_info_to_label('animal'),
+        ])
+
+    def test_str(self):
+        entry = DictionaryEntry.objects.create(
+            jp_text="犬",
+            en_text="dog",
+            meta_text="(n)",
+            sequence_number=100,
+            source_import_request=create_stub_import_request(),
+        )
+
+        self.assertEqual(str(entry), "犬|dog")
