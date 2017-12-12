fb66c3096fc93ee114e2b2d68d83f3b872bd35b3
Christopher Thorne <libcthorne@gmail.com>
2017-12-12 20:17:54 +0000

    Add Profile model tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import(
+    Profile,
+    User,
+)
+
+def get_stub_user():
+    return User.objects.create()
+
+class ProfileTests(TestCase):
+    def test_get_default_image_url(self):
+        profile = Profile.objects.create(
+            user=get_stub_user(),
+            display_name="test",
+        )
+
+        url = profile.get_image_or_default_url()
+        self.assertEqual(url, "/static/images/default_profile_image.png")
+
+    def test_get_custom_image_url(self):
+        profile = Profile.objects.create(
+            user=get_stub_user(),
+            display_name="test",
+            image="images/hello.jpg",
+        )
+
+        url = profile.get_image_or_default_url()
+        self.assertEqual(url, "/uploads/images/hello.jpg")
+
+    def test_str(self):
+        profile = Profile.objects.create(
+            user=get_stub_user(),
+            display_name="My display name",
+        )
+
+        self.assertEqual(str(profile), "My display name")
