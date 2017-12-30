9b391bbfa32acae427520a8cf21d62b77b7b2c91
Christopher Thorne <libcthorne@gmail.com>
2017-12-30 19:02:32 +0000

    Add cthorne.me and subdomains to ALLOWED_HOSTS

--- edictweb/settings.py
+++ edictweb/settings.py
@@ -27,7 +27,7 @@ DEBUG = os.environ.get('DJANGO_DEBUG', False)
 
 INTERNAL_IPS = ['localhost', '127.0.0.1', '10.0.2.2']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.cthorne.me']
 
 
 # Application definition
