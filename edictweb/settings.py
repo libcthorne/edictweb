b0de440e0e7b4701b9af49c196d1654bd92e0da0
Christopher Thorne <libcthorne@gmail.com>
2017-12-31 15:37:19 +0000

    Fix ALLOWED_HOSTS breaking local site instance

--- edictweb/settings.py
+++ edictweb/settings.py
@@ -27,7 +27,9 @@ DEBUG = os.environ.get('DJANGO_DEBUG', False)
 
 INTERNAL_IPS = ['localhost', '127.0.0.1', '10.0.2.2']
 
-ALLOWED_HOSTS = ['.cthorne.me']
+ALLOWED_HOSTS = [] if DEBUG else [
+    '.cthorne.me',
+]
 
 
 # Application definition
