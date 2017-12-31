7f18b062f797b15b5387517f3695ad8769c2b3da
Christopher Thorne <libcthorne@gmail.com>
2017-12-31 15:23:49 +0000

    Fix duplicate meta labels showing

--- importer/models.py
+++ importer/models.py
@@ -29,13 +29,13 @@ class DictionaryEntry(models.Model):
 
     @property
     def meta_labels(self):
-        meta_labels = []
+        meta_labels = set()
         meta_text_stripped = re.sub("[\(\){}]", "", self.meta_text)
         meta_text_groups = meta_text_stripped.split(" ")
         for meta_text_group in meta_text_groups:
             meta_infos = meta_text_group.split(",")
             for meta_info in meta_infos:
-                meta_labels.append(meta_info_to_label(meta_info))
+                meta_labels.add(meta_info_to_label(meta_info))
         return meta_labels
 
     def __str__(self):
