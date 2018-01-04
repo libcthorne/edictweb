c5c4d309a6d43ec2af2a7a236456b9b74380b0a9
Christopher Thorne <libcthorne@gmail.com>
2018-01-04 20:45:07 +0000

    Revert "Adjust frequency rank weight significance"
    This reverts commit 278a0eb3df0795985d84571dee6e4e99a96be1ec.

--- importer/tasks.py
+++ importer/tasks.py
@@ -108,7 +108,7 @@ def _build_index_entries(dictionary_entry, descriptions, index_column):
                 # Common words should have larger weight
                 if dictionary_entry.frequency_rank:
                     frequency_scale = max(51-dictionary_entry.frequency_rank, 1)/50
-                    weight += 1.5*frequency_scale
+                    weight += 2*frequency_scale
 
                 inverted_index_entry = InvertedIndexEntry(
                     index_word_text=word_ngram,
