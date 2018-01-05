ccf37520b2dc6c5e9617c42070ce739e20bb8e6b
Christopher Thorne <libcthorne@gmail.com>
2018-01-05 20:03:31 +0000

    Adjust weight to account for index of word and description matches

--- importer/tasks.py
+++ importer/tasks.py
@@ -44,7 +44,7 @@ def index_dictionary_entry_by_id(dictionary_entry_id):
     en_text_glosses = en_text.split("/")
 
     # Build index entries from forms and glosses
-    entries = _build_index_entries(dictionary_entry, jp_text_forms + en_text_glosses)
+    entries = _build_index_entries(dictionary_entry, [jp_text_forms, en_text_glosses])
 
     # Save index entries
     save_start = datetime.now()
@@ -53,52 +53,59 @@ def index_dictionary_entry_by_id(dictionary_entry_id):
 
     print("Saved {} index entries in {}".format(len(entries), save_end-save_start))
 
-def _build_index_entries(dictionary_entry, descriptions):
+def _build_index_entries(dictionary_entry, descriptions_collection):
     """
     Builds InvertedIndexEntry objects for a given dictionary entry
-    from a list of descriptions. Descriptions can be a list of English
-    definitions ("glosses") with words separated by spaces, e.g.
-    ["stray cat", "alley cat"] or a list of Japanese forms, e.g.
-    ["野良猫", "のらねこ"], or both combined.
+    from a collection of descriptions. Descriptions can be a list of
+    English definitions ("glosses") with words separated by spaces,
+    e.g.  ["stray cat", "alley cat"] or a list of Japanese forms, e.g.
+    ["野良猫", "のらねこ"].
     """
     # Collection of index entries built, mapped by word
     entries = {}
 
-    for raw_description in descriptions:
-        description = normalize_query(raw_description)
-        description_words = description.split(' ')
-        for raw_word in description_words:
-            word = normalize_word(raw_word)
-            if not word:
-                continue # word is not indexable
+    for descriptions in descriptions_collection:
+        for description_index, raw_description in enumerate(descriptions):
+            description = normalize_query(raw_description)
+            description_words = description.split(' ')
+            for word_index, raw_word in enumerate(description_words):
+                word = normalize_word(raw_word)
+                if not word:
+                    continue # word is not indexable
 
-            # Index each edge n-gram (i.e. quick -> q, qu, qui, quic, quick)
-            for i in range(len(word)):
-                word_ngram = word[:i+1]
+                # Index each edge n-gram (i.e. quick -> q, qu, qui, quic, quick)
+                for i in range(len(word)):
+                    word_ngram = word[:i+1]
 
-                weight = 1.0
+                    weight = 1.0
 
-                match_rate = len(word_ngram)/len(word)
-                if match_rate == 1:
-                    # Exact matches should have larger weight
-                    weight += 3
-                else:
-                    # Partial matches should have smaller weight
-                    weight += 1*match_rate
+                    match_rate = len(word_ngram)/len(word)
+                    if match_rate == 1:
+                        # Exact matches should have larger weight
+                        weight += 3
+                    else:
+                        # Partial matches should have smaller weight
+                        weight += 1*match_rate
 
-                # Matches in a long description should have smaller weight
-                weight += 1*((4-min(len(description_words), 3))/3)
+                    # Matches in short descriptions should have larger weight
+                    weight += 1*(max(4-len(description_words), 1)/3)
 
-                # Common words should have larger weight
-                if dictionary_entry.frequency_rank:
-                    frequency_scale = max(51-dictionary_entry.frequency_rank, 1)/50
-                    weight += 2*frequency_scale
+                    # Matches in earlier occurring words should have larger weight
+                    weight += 1*(max(3-word_index, 1)/3)
 
-                if word_ngram not in entries or weight > entries[word_ngram].weight:
-                    entries[word_ngram] = InvertedIndexEntry(
-                        index_word_text=word_ngram,
-                        dictionary_entry=dictionary_entry,
-                        weight=weight,
-                    )
+                    # Matches in earlier occurring descriptions should have larger weight
+                    weight += 2*((len(descriptions)-description_index)/len(descriptions))
+
+                    # Common words should have larger weight
+                    if dictionary_entry.frequency_rank:
+                        frequency_scale = max(51-dictionary_entry.frequency_rank, 1)/50
+                        weight += 2*frequency_scale
+
+                    if word_ngram not in entries or weight > entries[word_ngram].weight:
+                        entries[word_ngram] = InvertedIndexEntry(
+                            index_word_text=word_ngram,
+                            dictionary_entry=dictionary_entry,
+                            weight=weight,
+                        )
 
     return list(entries.values())
