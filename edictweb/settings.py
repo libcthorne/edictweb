6ca2198918f7bfb0b9b53b5a40c80cc366376e89
Christopher Thorne <libcthorne@gmail.com>
2018-01-07 10:35:06 +0000

    Revert "Change ALLOWED_HOSTS to always include .cthorne.me"
    This reverts commit 45565016b931a4dd6b9f67764627a7f07a781952.

--- edictweb/settings.py
+++ edictweb/settings.py
@@ -27,12 +27,9 @@ DEBUG = os.environ.get('DJANGO_DEBUG', False)
 
 INTERNAL_IPS = ['localhost', '127.0.0.1', '10.0.2.2']
 
-ALLOWED_HOSTS = [
+ALLOWED_HOSTS = [] if DEBUG else [
     '.cthorne.me',
 ]
-
-if DEBUG:
-    ALLOWED_HOSTS += INTERNAL_IPS
 
 
 # Application definition
