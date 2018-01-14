3076a6bbeb511ea1574274cfe3c10ad34e2f13a7
Christopher Thorne <libcthorne@gmail.com>
2018-01-14 10:07:13 +0000

    Remove unused code from accounts app

--- accounts/forms.py
+++ accounts/forms.py
@@ -13,19 +13,6 @@ class AuthenticationForm(default_auth_forms.AuthenticationForm):
         self.fields['password'].widget.attrs['placeholder'] = "Password"
         self.fields['password'].widget.attrs['class'] = "form-control"
 
-class UserCreationForm(default_auth_forms.UserCreationForm):
-    def __init__(self, *args, **kwargs):
-        super(UserCreationForm, self).__init__(*args, **kwargs)
-
-        self.fields['username'].widget.attrs['placeholder'] = "Username"
-        self.fields['username'].widget.attrs['class'] = "form-control"
-
-        self.fields['password1'].widget.attrs['placeholder'] = "Password"
-        self.fields['password1'].widget.attrs['class'] = "form-control"
-
-        self.fields['password2'].widget.attrs['placeholder'] = "Password (confirmation)"
-        self.fields['password2'].widget.attrs['class'] = "form-control"
-
 class ProfileForm(ModelForm):
     class Meta:
         model = Profile
